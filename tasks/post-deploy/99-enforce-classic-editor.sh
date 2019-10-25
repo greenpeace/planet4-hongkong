93323bea0bc47fd29fb03daaa08c18832b4d3e4a
Nikos Roussos <nikos@roussos.cc>
2019-10-25 13:19:04 +0300

    Add non-gutenberg p4 packages
    Also enforce Classic editor

---
+++ tasks/post-deploy/99-enforce-classic-editor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Deactivate p4 gutenberg plugins
+wp plugin deactivate planet4-plugin-gutenberg-blocks
+wp plugin deactivate planet4-plugin-gutenberg-engagingnetworks
+
+# Set classic editor as the default editor
+wp option update classic-editor-replace 'classic'
+
+# Disallow users to switch editors (classic - gutenberg)
+wp option update classic-editor-allow-users 'disallow'
